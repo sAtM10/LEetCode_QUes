8b2fae7353bc98117f2dd072b286fd685d6a32af
satwik <100366942+sAtM10@users.noreply.github.com>
2023-07-08 13:38:49 +0530

    Create Fib.cpp

---
+++ Fib.cpp
@@ -0,0 +1,12 @@
+// 509. Fibonacci Number
+//Using multiple Recursion 
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1) return n;
+        int last = fib(n-1);
+        int slast = fib(n-2);
+
+        return last + slast;
+    }
+};
