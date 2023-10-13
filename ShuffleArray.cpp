6cce35141ee8eb8955a8975c6640820b4507eeb8
satwik <100366942+sAtM10@users.noreply.github.com>
2023-10-14 01:45:54 +0530

    Create ShuffleArray.cpp

---
+++ ShuffleArray.cpp
@@ -0,0 +1,13 @@
+//1470. Shuffle the Array
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+                vector<int> v;
+        for(int x=0; x<n; x++)
+        {
+            v.push_back(nums[x]);
+            v.push_back(nums[n+x]);
+        }
+        return v;
+    }
+};
