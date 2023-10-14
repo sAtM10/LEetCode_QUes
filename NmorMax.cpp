e2e7961c52f6af8b55613c358cc4e888bf5d8dca
satwik <100366942+sAtM10@users.noreply.github.com>
2023-10-14 22:44:58 +0530

    Create NmorMax.cpp

---
+++ NmorMax.cpp
@@ -0,0 +1,13 @@
+//2733. Neither Minimum nor Maximum
+        int n = nums.size();
+        int minn = nums[0], maxx = nums[0];
+
+        for (int i=1; i<n; i++) {
+            minn = min(minn, nums[i]);
+            maxx = max(maxx, nums[i]);
+        }
+
+        for (auto i: nums) 
+            if (i != minn && i != maxx) return i;
+
+        return -1;
