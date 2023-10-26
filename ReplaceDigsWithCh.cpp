eda31120b986b589e8925b266b3cb6079b6e82af
satwik <100366942+sAtM10@users.noreply.github.com>
2023-10-26 09:19:19 +0530

    Create ReplaceDigsWithCh.cpp

---
+++ ReplaceDigsWithCh.cpp
@@ -0,0 +1,11 @@
+//1844. Replace All Digits with Characters
+class Solution {
+public:
+    string replaceDigits(string s) {
+        for(int i=1;i<s.length();i=i+2) 
+      {int temp=s[i]-'0';
+          s[i]=s[i-1]+temp;
+      }
+      return s;
+    }
+};
